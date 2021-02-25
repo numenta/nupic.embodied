d4d8b749f77d20695156bce49e5ff586063862ed
Lucas <lucasosouza@gmail.com>
2021-02-24 18:15:06 -0800

    reverting accidental change in the script

--- projects/robot_arm/arm_evaluate.py
+++ projects/robot_arm/arm_evaluate.py
@@ -38,10 +38,11 @@ result, detailed_scores = real_robots.evaluate(
     visualize=False,
     goals_dataset_path="./goals/goals-REAL2020-sNone-25-15-10-3.npy.npz",
 )
+
 # NOTE : You can find goals-REAL2020-s2020-50-1.npy.npz file in the REAL2020 Starter Kit
 # repository or you can generate one using the real-robots-generate-goals command.
-#
-print(result, result, result, result, result, result, result, result, result, result)
+
+print(result)
 # {'score_REAL2020': 0.06529471503519801, 'score_total': 0.06529471503519801}
 print(detailed_scores)
 # {'REAL2020': [0.00024387094790936833, 0.19553060745741896, 0.00010966670026571288]}
