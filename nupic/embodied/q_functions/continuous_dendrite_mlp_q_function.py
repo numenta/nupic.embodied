3348c797a82602c5d5cd4e5cae1693e2740e48ee
Akash Velu <akashvelu@berkeley.edu>
2021-03-12 12:54:18 -0800

    Flake8 and formatting

--- nupic/embodied/q_functions/continuous_dendrite_mlp_q_function.py
+++ nupic/embodied/q_functions/continuous_dendrite_mlp_q_function.py
@@ -1,4 +1,25 @@
-"""This modules creates a continuous Q-function network."""
+# ------------------------------------------------------------------------------
+#  Numenta Platform for Intelligent Computing (NuPIC)
+#  Copyright (C) 2021, Numenta, Inc.  Unless you have an agreement
+#  with Numenta, Inc., for a separate license for this software code, the
+#  following terms and conditions apply:
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero Public License version 3 as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#  See the GNU Affero Public License for more details.
+#
+#  You should have received a copy of the GNU Affero Public License
+#  along with this program.  If not, see http://www.gnu.org/licenses.
+#
+#  http://numenta.org/licenses/
+#
+# ------------------------------------------------------------------------------
+"""This modules creates a continuous Q-function network with a Dendrite MLP."""
 
 import torch
 
@@ -42,5 +63,6 @@ class ContinuousDendriteMLPQFunction(DendriticMLP):
         Returns:
             torch.Tensor: Output value
         """
-        obs_portion, context_portion = observations[:, :self._obs_dim], observations[:, self._obs_dim:]
+        obs_portion = observations[:, :self._obs_dim]
+        context_portion = observations[:, self._obs_dim:]
         return super().forward(torch.cat([obs_portion, actions], 1), context_portion)
