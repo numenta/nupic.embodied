47ba07c5d546353ac7c43bc95ee979672911edf1
Akash Velu <akashvelu@berkeley.edu>
2021-03-15 11:10:31 -0700

    Add SAC algorithm classes which include rezero_weights calls

--- nupic/embodied/algos/sparse_weights_sac.py
+++ nupic/embodied/algos/sparse_weights_sac.py
@@ -1,3 +1,24 @@
+# ------------------------------------------------------------------------------
+#  Numenta Platform for Intelligent Computing (NuPIC)
+#  Copyright (C) 2021, Numenta, Inc.  Unless you have an agreement
+#  with Numenta, Inc., for a separate license for this software code, the
+#  following terms and conditions apply:
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero Public License version 3 as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#  See the GNU Affero Public License for more details.
+#
+#  You should have received a copy of the GNU Affero Public License
+#  along with this program.  If not, see http://www.gnu.org/licenses.
+#
+#  http://numenta.org/licenses/
+#
+# ------------------------------------------------------------------------------
 from garage.torch.algos.sac import SAC
 from nupic.torch.modules.sparse_weights import rezero_weights
 
@@ -62,3 +83,9 @@ class SparseWeightsSAC(SAC):
             self._alpha_optimizer.step()
 
         return policy_loss, qf1_loss, qf2_loss
+
+    def _evaluate_policy(self, epoch):
+        self.policy.eval()
+        result = super(SparseWeightsSAC, self)._evaluate_policy(epoch=epoch)
+        self.policy.train()
+        return result
