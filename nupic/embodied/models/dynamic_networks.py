cf748f576f41d2a880d1f24be380d0699d6c017b
Lucas <lucasosouza@gmail.com>
2021-04-08 22:40:41 -0700

    refactored version of the dynamics network

---
+++ nupic/embodied/models/dynamic_networks.py
@@ -0,0 +1,124 @@
+# ------------------------------------------------------------------------------
+#  Numenta Platform for Intelligent Computing (NuPIC)
+#  Copyright (C) 2021, Numenta, Inc.  Unless you have an agreement
+#  with Numenta, Inc., for a separate license for this software code, the
+#  following terms and conditions apply:
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero Public License version 3 as
+#  published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#  See the GNU Affero Public License for more details.
+#
+#  You should have received a copy of the GNU Affero Public License
+#  along with this program.  If not, see http://www.gnu.org/licenses.
+#
+#  http://numenta.org/licenses/
+#
+# ------------------------------------------------------------------------------
+import torch
+from torch import nn
+
+from functools import partial
+
+class DynamicsLeakyRelu(nn.LeakyReLU):
+    """Default activation function for Dynamic Networks"""
+    def forward(self, x, ac):
+        return super().forward(x), ac
+
+class DynamicsLinear(nn.Linear):
+    """Default linear layer for Dynamic Networks"""
+    def forward(self, x, ac):
+        x = torch.cat((x, ac), dim=-1)
+        return super().forward(x), ac
+
+class DynamicsSequential(torch.nn.Sequential):
+    """Sequential for Dynamic Networks that accepts action"""
+    def forward(self, x, ac):
+        for module in self:
+            x, _ = module(x, ac)
+        return x, ac
+
+
+class DynamicsBlock(DynamicsSequential):
+    """
+    Default hidden block for Dynamic Networks
+    Two linear layers each, the first layer uses a non-linear activation function.
+    """
+
+    def __init__(self, hidden_dim, ac_dim, activation_fn):
+        super().__init__(
+            DynamicsLinear(hidden_dim + ac_dim, hidden_dim),
+            activation_fn(),
+            DynamicsLinear(hidden_dim + ac_dim, hidden_dim)
+        )
+
+    def forward(self, x, ac):
+        out, _ = super().forward(x, ac)
+        return out + x, ac
+
+
+class DynamicsNet(torch.nn.Module):
+    """Residual network to get the dynamics loss using the features from the auxiliary
+    task model.
+
+    :param nblocks: Number of residual blocks in the dynamics network.
+    :param feat_dim: Number of features from the feature network.
+    :param ac_dim: Action dimensionality.
+    :param out_feat_dim: Number of features from the feature network for the next state
+                        (usually same).
+    :param hid_dim: Number of neurons in the hidden layers.
+    :param activation_fn: Activation function factory.
+    """
+
+    leaky_relu = partial(DynamicsLeakyRelu, negative_slope=0.2)
+
+    def __init__(
+        self,
+        nblocks,
+        feat_dim,
+        ac_dim,
+        out_feat_dim,
+        hid_dim,
+        activation_fn=leaky_relu,
+    ):
+        super().__init__()
+
+        # First layer of the model takes state features + actions as input and outputs
+        # hid_dim activations
+        self.input = DynamicsSequential(
+            DynamicsLinear(feat_dim + ac_dim, hid_dim),
+            activation_fn(),
+        )
+
+        # n residual blocks
+        self.hidden = DynamicsSequential(
+            *[DynamicsBlock(hid_dim, ac_dim, activation_fn) for _ in range(nblocks)]
+        )
+
+        self.output = DynamicsLinear(hid_dim + ac_dim, out_feat_dim)
+
+        self.init_weight()
+
+    def init_weight(self):
+        """Initialize the weights with xavier (glorot) uniform initialization"""
+        for module in self.modules():
+            if isinstance(module, torch.nn.Linear):
+                torch.nn.init.xavier_uniform_(module.weight.data)
+                torch.nn.init.constant_(module.bias.data, 0.0)
+
+    def forward(self, x, ac):
+        """Get output of a forward pass through the dynamics network.
+
+        :param features: Features from the auxiliary network corresponding with the
+                         current state
+        :param ac: Current actions.
+        :return: Features of residual dynamics model from the current states & actions
+        """
+        x, _ = self.input(x, ac)
+        x, _ = self.hidden(x, ac)
+        x, _ = self.output(x, ac)
+        return x
