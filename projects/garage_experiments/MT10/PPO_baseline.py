2f89724f00ba5120aa43b0a3597964f4893cdd53
Akash Velu <akashvelu@berkeley.edu>
2021-05-03 11:06:01 -0700

    Match baseline MT10 version to the TF version described in the metaworld slack

--- projects/garage_experiments/MT10/PPO_baseline.py
+++ projects/garage_experiments/MT10/PPO_baseline.py
@@ -3,20 +3,14 @@
 # pylint: disable=no-value-for-parameter
 import click
 import metaworld
-import torch
 import wandb
 
 from garage import wrap_experiment
 from garage.envs import normalize
-from nupic.embodied.envs.garage_wrappers.multi_env_taskid_wrapper import MultiEnvTaskWrapper
-from garage.envs.multi_env_wrapper import MultiEnvWrapper, round_robin_strategy
 from garage.experiment import MetaWorldTaskSampler
 from garage.experiment.deterministic import set_seed
-from garage.sampler import RaySampler, EvalWorker, DefaultWorker
-from garage.torch.algos import PPO
+from garage.sampler import RaySampler, DefaultWorker
 from nupic.embodied.algos.custom_mt_ppo import CustomMTPPO
-from garage.torch.policies import GaussianMLPPolicy
-from garage.torch.value_functions import GaussianMLPValueFunction
 from garage.trainer import Trainer
 from nupic.embodied.utils.utils import get_params, create_policy_net, create_vf_net
 
@@ -67,14 +61,8 @@ def mtppo_metaworld_mt10(ctxt, experiment_name, config_pth, seed, n_workers, n_t
     assert n_tasks % 10 == 0
     assert n_tasks <= 500
 
-    # MT10_train_envs = train_task_sampler.sample(n_tasks)
-    # env = MT10_train_envs[0]()
     envs = [env_up() for env_up in train_task_sampler.sample(n_tasks)]
-    # task_envs = [MultiEnvTaskWrapper(envs, i, sample_strategy=round_robin_strategy, mode='vanilla') for i in range(n_tasks)]
-    # example_env = task_envs[0]
-    env = MultiEnvWrapper(envs,
-                          sample_strategy=round_robin_strategy,
-                          mode='vanilla')
+    env = envs[0]
 
     policy = create_policy_net(env_spec=env.spec,
                                net_params=params["net"]
@@ -89,17 +77,13 @@ def mtppo_metaworld_mt10(ctxt, experiment_name, config_pth, seed, n_workers, n_t
                          n_workers=n_workers,
                          worker_class=DefaultWorker)
 
-    eval_sampler = RaySampler(agents=policy,
-                              envs=env,
-                              max_episode_length=env.spec.max_episode_length
-                              , n_workers=n_workers,
-                              worker_class=EvalWorker)
-
     algo = CustomMTPPO(env_spec=env.spec,
                        policy=policy,
                        value_function=value_function,
                        sampler=sampler,
-                       test_sampler=eval_sampler,
+                       train_task_sampler=train_task_sampler,
+                       num_tasks=n_tasks,
+                       task_update_frequency=params["training"]["task_update_frequency"],
                        num_eval_eps=params["general_setting"]["eval_episodes"],
                        policy_lr=params["training"]["policy_lr"],
                        vf_lr=params["training"]["vf_lr"],
@@ -109,13 +93,19 @@ def mtppo_metaworld_mt10(ctxt, experiment_name, config_pth, seed, n_workers, n_t
                        num_train_per_epoch=params["training"]["num_train_per_epoch"],
                        discount=params["general_setting"]["discount"],
                        gae_lambda=params["training"]["gae_lambda"],
-                       center_adv=True,
+                       center_adv=False,
                        wandb_logging=use_wandb,
-                       eval_freq=params["general_setting"]["eval_freq"])
+                       eval_freq=params["general_setting"]["eval_freq"],
+                       stop_entropy_gradient=True,
+                       entropy_method='max',
+
+                       )
 
     trainer = Trainer(ctxt)
     trainer.setup(algo, env)
-    trainer.train(n_epochs=params["training"]["epochs"], batch_size=params["training"]["batch_size"])
+    trainer.train(n_epochs=params["training"]["epochs"],
+                  batch_size=params["training"]["batch_episodes_per_task"],
+                  plot=False)
 
 
 if __name__ == '__main__':
